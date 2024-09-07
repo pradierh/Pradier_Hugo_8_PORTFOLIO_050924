9265e41a78416635f94c2fbe46ebf65ea1cc8849
Hugo Pradier <hpradier@icloud.com>
2024-09-07 17:45:06 +0200

    try

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,4 @@
-import { withSentryConfig } from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = { output: 'export' };
 
-export default withSentryConfig(
-	nextConfig,
-	{
-		silent: true,
-		org: 'javascript-mastery',
-		project: 'javascript-nextjs',
-	},
-	{
-		widenClientFileUpload: true,
-		transpileClientSDK: true,
-		hideSourceMaps: true,
-		disableLogger: true,
-
-		automaticVercelMonitors: true,
-	}
-);
+export default nextConfig;
