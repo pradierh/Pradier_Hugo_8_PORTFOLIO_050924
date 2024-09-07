e4573bc9673a763c38bf3ee667642ca6fe4067ea
Hugo Pradier <hpradier@icloud.com>
2024-09-07 17:02:04 +0200

    config file change

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,20 @@
-import { output } from 'three/webgpu';
-
+import { withSentryConfig } from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: 'export' };
+const nextConfig = {};
 
-export default nextConfig;
+export default withSentryConfig(
+	nextConfig,
+	{
+		silent: true,
+		org: 'javascript-mastery',
+		project: 'javascript-nextjs',
+	},
+	{
+		widenClientFileUpload: true,
+		transpileClientSDK: true,
+		hideSourceMaps: true,
+		disableLogger: true,
+
+		automaticVercelMonitors: true,
+	}
+);
